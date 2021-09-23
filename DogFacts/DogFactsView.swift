af4a559f67476e6dfe22c7fea757a57c467d3244
rokas_m <rokas1899@gmail.com>
2021-09-23 11:45:21 +0300

    Design

--- DogFacts/DogFactsView.swift
+++ DogFacts/DogFactsView.swift
@@ -11,15 +11,46 @@ struct DogFactsView: View {
     @ObservedObject var viewModel: DogFactsViewModel = DogFactsViewModel()
     
     var body: some View {
-        switch viewModel.apiState {
-        case .loading:
-            ProgressView()
-                .frame(maxHeight: .infinity)
-        case .success:
-            Text(viewModel.fact)
-                .padding()
-        case .failed:
-            Text("Couldn't retrieve data")
+        ZStack {
+            Rectangle().fill(
+                LinearGradient(gradient: Gradient(
+                                colors: [Color("Background1"), Color("Background2")]),
+                               startPoint: .topTrailing,
+                               endPoint: .bottomLeading))
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Spacer()
+                switch viewModel.apiState {
+                case .loading:
+                    ProgressView()
+                        .frame(maxHeight: .infinity)
+                case .success:
+                    Text(viewModel.fact)
+                        .font(.system(size: 27, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                        .shadow(color: Color.black.opacity(0.25), radius: 0.1, x: -1 , y: 1)
+                        .transition(.slide)
+                        .padding(15)
+                        .multilineTextAlignment(.center)
+                case .failed:
+                    Text("Couldn't retrieve data")
+                }
+                Spacer()
+                Button(action: {
+                    withAnimation {
+                        viewModel.fetchDogFact()
+                    }
+                }) {
+                    Text("Another one!")
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                        .padding(10)
+                        .background(Color.white.opacity(0.25))
+                }
+                Spacer()
+            }
+            
         }
     }
 }
