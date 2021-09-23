930283ac4d1c5dd18b7612f034d694ec41640adb
rokas_m <rokas1899@gmail.com>
2021-09-23 10:33:59 +0300

    init

---
+++ DogFacts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DogFacts
+//
+//  Created by Rokas Mikelionis on 2021-09-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DogFacts/DogFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogFactsApp.swift
+//  DogFacts
+//
+//  Created by Rokas Mikelionis on 2021-09-23.
+//
+
+import SwiftUI
+
+@main
+struct DogFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
