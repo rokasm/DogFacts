80ee3b41d47940ef35f096b82e0a1e2b2ec9b761
rokas_m <rokas1899@gmail.com>
2021-10-07 23:37:36 +0300

    swinject

--- DogFacts/DogFactsViewModel.swift
+++ DogFacts/DogFactsViewModel.swift
@@ -6,13 +6,31 @@
 //
 
 import Foundation
-import Combine
+import Swinject
+import SwinjectAutoregistration
 
 class DogFactsViewModel: ObservableObject {
     @Published private(set) var dogFact: DogFact?
-    var fetcher = DogFactFetcher(network: ApiRequest(), storageManager: DogFactsStorageManager())
+
+    var fetcher: DogFactFetcher
     
     init() {
+        let container = Container()
+
+        container.register(ApiRequest.self) { _ in
+          return ApiRequest()
+        }
+        
+        container.register(DogFactsStorageManager.self) { _ in
+          return DogFactsStorageManager()
+        }
+        
+        container.register(DogFactFetcher.self) { resolver in
+            let network = resolver.resolve(ApiRequest.self)!
+            let storage = resolver.resolve(DogFactsStorageManager.self)!
+            return DogFactFetcher(network: network, storageManager: storage)
+        }
+        fetcher = container.resolve(DogFactFetcher.self)!
         apiState = .loading
         fetchDogFact()
     }
